6c4ada12e03f40b61a185ad0607566ead46d2d1e
dfountain0 <don@hackthehood.org>
2017-11-06 21:49:55 -0800

    Part 1 finished, Changed Language codes and edited setLanguage Function

--- hw04/scripts/part1.js
+++ hw04/scripts/part1.js
@@ -1,13 +1,15 @@
 let language
 let languageCode
-
+/*The setLanguage function cchanges the  language of the page from Russion,English,
+  or Spanish depending on what language code the code variable holds based off what
+  Language the user clicks on*/
 const setLanguage = (code) => {
     //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
     languageCode = code
-    if (code === 'ru') {
-        language = 'Russian'
-    } else if (code === 'es') {
-        language = 'Spanish'
+    if (code === 'ht') {
+        language = 'Hatian Creole'
+    } else if (code === 'ar') {
+        language = 'Arabic'
     } else {
         language = 'English'
     }
